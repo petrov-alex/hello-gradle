926c2542d37364a314a869d0351c228b9c9782b3
Alex Petrov <petrov.alex.master@yandex.ru>
2021-08-08 18:16:27 +0300

    second commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,12 @@
+import org.apache.commons.lang3.StringUtils;
+
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello");
+
+    System.out.println(StringUtils.capitalize("one two three"));
+    System.out.println(StringUtils.upperCase("one two three"));
+    System.out.println(StringUtils.upperCase("one two three"));
+    System.out.println(StringUtils.upperCase("one two three"));
+  }
+}
